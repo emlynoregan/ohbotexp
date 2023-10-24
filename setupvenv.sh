35ed583a88b546d7d17fd7f43fff9e26967fa4d5
Emlyn O'Regan <emlynoregan@gmail.com>
2023-10-24 12:56:09 +1030

    stuff

--- setupvenv.sh
+++ setupvenv.sh
@@ -1,5 +1,8 @@
 # need to invoke this file from the root of the project, using "source"
+sudo chmod 666 /dev/ttyACM0
 sudo chmod 666 /dev/ttyACM1
+sudo chmod 666 /dev/ttyACM2
+sudo chmod 666 /dev/ttyACM3
 python3 -m venv ./venv
 source ./venv/bin/activate
 python -m pip install --upgrade pip
